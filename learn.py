4db722a74dadf148b96e06c26fc36ffd58399627
feathershine <zhl13779081529@hotmail.com>
2018-07-13 12:53:38 +0800

    更新readme

--- learn.py
+++ learn.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
+# /usr/bin/python3
 # -*- coding=utf-8 -*-
 
 # 这里是python学习过程中的总结
 
 # author: feathershine
 print('hello world!')
+
+name = input()
+print(name)
 
 # feathershine
