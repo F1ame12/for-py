7e8f1a0fb30b32c4bd218c532e5b0800c4bc9284
youngniu <codeniu@163.com>
2018-07-14 09:55:38 +0800

    update

--- py-learn-code/learnday02.py
+++ py-learn-code/learnday02.py
@@ -1,5 +1,23 @@
 #youngniu
+
+#1.test print()
 value=10
 number=20
 print("print函数的用法",value,number,sep="，",end="。",flush=False)
+
+#2.test str
+str1="my name is niu jingxiang"
+str2="""I'm a 18's old boy"""
+print("max(str)的用法：",max(str1),"len(str)的用法：",len(str1))
+print("min()的用法：",min(str1))
+
+#3.字符串运算
+str_link = "niu"+"jingxiang"
+print("1.str+str:",str_link)
+str_link+=str_link
+print("2.str+=:",str_link)
+print("3.str_link*3:",str_link*3)
+
+
 #youngniu
+
