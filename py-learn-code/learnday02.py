f2c64159a6a73d6d61e7e03af53d2dc0ff66c488
feathershine <zhl13779081529@hotmail.com>
2018-07-14 10:04:00 +0800

    update feathershine

--- py-learn-code/learnday02.py
+++ py-learn-code/learnday02.py
@@ -32,7 +32,6 @@ print('the input str\'s len is',len(str_test))
 print('ths str max char is', max(str_test))
 print('the str min char is', min(str_test))
 print(str(123456))# 将其他数据类型转为字符串
-<<<<<<< HEAD
 str_test *= 3 #重复字符串
 str_test += 'test' #连接字符串
 print('now the str is', str_test)
@@ -43,6 +42,3 @@ print('the list len is', len(str_list))
 
 
 # feathershine
-=======
-# feathershine
->>>>>>> b894e11bae6d79ce81b7c7759d496da6cf0ccd0a
