370569e3168d1a7cb5b3e1d63ac22c9a838fa8ed
feathershine <zhl13779081529@hotmail.com>
2018-07-14 19:00:27 +0800

    2018年7月14日作业更新

--- py-learn-code/作业/2018-7-14.py
+++ py-learn-code/作业/2018-7-14.py
@@ -1,3 +1,30 @@
+'''
+s="HelloPython"  
+1、 max(s)   #    
+2、 "python" in s  #  
+3、 "AB">="123"  #   
+4、 "AB" < "ABC"  #  
+5、 "ABC" == "abc"  #   
+6、 s[2:4]  #    
+7、 s[:]   #    
+8、 s[2:2]  #   
+9、 s[len(s)-2]    
+10、字符 'F' 的ASCII码为   #  
+11. s.lower()   #
+12. s.upper()   #
+13. s.isdigit()   #
+14. s.isalpha()   #
+15. s.isupper()   #
+16. s.rstrip()   #
+17. s.find("h",5)   #
+18. s.replace("h","H",1)   #
+19.输入一个字符串，打印这个字符串的第一个字符,中间的一个字符和最后一个字符。
+20.任意输入一个字符串，计算要输入的字符"a"的个数，并打印出来
+  例: 请输入:  abcdabcabazzzzz  打印:  字符"a"的个数为:4    
+'''
+
+
+
 s = 'HelloPython'
 print('1.', max(s))
 print('2.', 'python' in s)
