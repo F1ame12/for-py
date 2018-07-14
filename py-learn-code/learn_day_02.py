6c377d7b37891933fff44ecb44168bda603b904b
Black-Chocolate <1692959384@qq.com>
2018-07-14 10:37:34 +0800

    sunzhuii

--- py-learn-code/learn_day_02.py
+++ py-learn-code/learn_day_02.py
@@ -1,8 +1,47 @@
+#!usr/bin/python3
+# -*- coding=utf-8 -*-
+
 #youngniu
+
+#1.test print()
 value=10
 number=20
 print("print函数的用法",value,number,sep="，",end="。",flush=False)
+
+#2.test str
+str1="my name is niu jingxiang"
+str2="""I'm a 18's old boy"""
+print("max(str)的用法：",max(str1),"len(str)的用法：",len(str1))
+print("min()的用法：",min(str1))
+
+#3.字符串运算
+str_link = "niu"+"jingxiang"
+print("1.str+str:",str_link)
+str_link+=str_link
+print("2.str+=:",str_link)
+print("3.str_link*3:",str_link*3)
+
 #youngniu
+
+# author: feathershine
+def random_str():
+    pass
+
+str_test = input()
+print('the input str\'s len is',len(str_test))
+print('ths str max char is', max(str_test))
+print('the str min char is', min(str_test))
+print(str(123456))# 将其他数据类型转为字符串
+str_test *= 3 #重复字符串
+str_test += 'test' #连接字符串
+print('now the str is', str_test)
+str_list = []
+str_list.append(str_test)
+print('the list len is', len(str_list))
+
+
+
+# feathershine
 
 #black-chocolate
 '''
@@ -70,6 +109,17 @@ print("输出",a,b,c,sep=",",end=".",flush=False)
         s[2]  #c
         s[-2] #c
     切片：（slice）
+        作用：
+            用于字符串中，从字符串序列中取出相应的元素，重新组成一个新的字符串序列
+        语法：
+            字符串【开始索引：结束索引：步长】
+        例如：
+            s = "ABCDE"
+            s[0,2]  #
+            s[2:4:1] #
+            s[2:2] #
+            s[2::1] #
+            s[2:0] #
 '''
     
 
