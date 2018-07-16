aed443dd266a92ddfa40b47f795ecfa7d521568d
Codeniu <codeniu@163.com>
2018-07-16 09:59:36 +0800

    添加练习代码文件夹

---
+++ py-learn-code/code-codeniu/练习代码/day04.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+
+#1.作用域
+'''
+def func1():
+	enclosing = 1#enclosing作用域
+	def fuc2():
+	local = 2#局部作用域
+'''
+#2.图形用户界面
+import tkinter
+from tkinter import *
+
+#创建一个窗口
+tk = tkinter.Tk()
+tk=Tk()
