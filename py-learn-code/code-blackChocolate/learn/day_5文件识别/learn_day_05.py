333ddb56e0284d279d29f3921024bc342e16e6a7
Black-Chocolate <1692959384@qq.com>
2018-07-17 13:44:39 +0800

    多提交

--- py-learn-code/code-blackChocolate/learn/day_5文件识别/learn_day_05.py
+++ py-learn-code/code-blackChocolate/learn/day_5文件识别/learn_day_05.py
@@ -5,19 +5,28 @@ import time
 import sys
 import os
 import win32con
-import win32
 import win32clipboard as w
 
 class GetTexts(object):
     '''
     把文字复制到剪切板
     '''
-
     def getText(self):
         #打开剪切板
         w.OpenClipboard()
-        d = w.GetClipboardData(win32com.CF_UNICODETEXT)
-        print(d)
+        d = w.GetClipboardData(win32con.CF_UNICODETEXT)
+        w.CloseClipboard()
+        return d
+
+    def setText(self,aStr):
+        w.OpenClipboard()
+        w.EmptyClipboard()
+        d = w.SetClipboardData(win32con.CF_UNICODETEXT,aStr)
+        w.CloseClipboard()
+
+#GetTexts().setText('123')
+d=GetTexts().getText()
+print(d)
         
 '''
 #截图方法
