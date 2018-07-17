34ed5b439daeeeff793e6542c05f7866ea9b76d7
Black-Chocolate <1692959384@qq.com>
2018-07-17 13:33:58 +0800

    第五天实习

--- py-learn-code/code-blackChocolate/learn/day_5文件识别/learn_day_05.py
+++ py-learn-code/code-blackChocolate/learn/day_5文件识别/learn_day_05.py
@@ -1,12 +1,38 @@
 #pip install keyboard  监控键盘
 import keyboard
+from PIL import ImageGrab
+import time
+import sys
+import os
+import win32con
+import win32
+import win32clipboard as w
 
+class GetTexts(object):
+    '''
+    把文字复制到剪切板
+    '''
+
+    def getText(self):
+        #打开剪切板
+        w.OpenClipboard()
+        d = w.GetClipboardData(win32com.CF_UNICODETEXT)
+        print(d)
+        
+'''
+#截图方法
 def screenShot():
     #截图的开始
     if keyboard.wait('alt+ctrl+a') == None:
         #截图的结束
         if keyboard.wait('enter')   == None:
-            print("截图结束")
-    #获取图片
-
+            time.sleep(0.01)
+            #获取图片
+            img  = ImageGrab.grabclipboard()
+            #保存到项目中
+            img.save("for-py/py-learn-code/code-blackChocolate/learn/day_5文件识别/picture.png")
+    for n in range(5):
+        print(sys.maxsize)
 screenShot()
+'''
+print(sys.path)
