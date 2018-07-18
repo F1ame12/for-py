01e09dd5986faa3363ff9d7bfc92f0ab72510714
Black-Chocolate <1692959384@qq.com>
2018-07-18 16:22:00 +0800

    服务器

--- py-learn-code/code-blackChocolate/learn/day_6wifi破解密码/password.py
+++ py-learn-code/code-blackChocolate/learn/day_6wifi破解密码/password.py
@@ -3,7 +3,7 @@ import itertools as its
 #密码组成元素
 words = "1234567890"
 #迭代[words] repeat位数（长度）
-r = its.product(words,repeat=6)
+r = its.product(words,repeat=4)
 #将密码保存文件中  打开[pass.txt]没有则创建，a是对文件的操作，表示追加
 dic = open('for-py/py-learn-code/code-blackChocolate/learn/day_6wifi破解密码/pass.txt','a')
 #将内容追加到文件
