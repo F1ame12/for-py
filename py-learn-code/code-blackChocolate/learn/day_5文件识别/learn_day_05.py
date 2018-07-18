701e1bf5616d37f95a9469a6a8545789cd05e255
Black-Chocolate <1692959384@qq.com>
2018-07-18 09:56:03 +0800

    第六天实习

--- py-learn-code/code-blackChocolate/learn/day_5文件识别/learn_day_05.py
+++ py-learn-code/code-blackChocolate/learn/day_5文件识别/learn_day_05.py
@@ -6,29 +6,8 @@ import sys
 import os
 import win32con
 import win32clipboard as w
-
-class GetTexts(object):
-    '''
-    把文字复制到剪切板
-    '''
-    def getText(self):
-        #打开剪切板
-        w.OpenClipboard()
-        d = w.GetClipboardData(win32con.CF_UNICODE=TEXT)
-        w.CloseClipboard()
-        return d
-
-    def setText(self,aStr):
-        w.OpenClipboard()
-        w.EmptyClipboard()
-        d = w.SetClipboardData(win32con.CF_UNICODETEXT,aStr)
-        w.CloseClipboard()
-
-#GetTexts().setText('123')
-d=GetTexts().getText()
-print(d)
-        
-'''
+from 文字识别 import BaiDuAPI
+from 剪切板 import GetTexts
 #截图方法
 def screenShot():
     #截图的开始
@@ -40,8 +19,19 @@ def screenShot():
             img  = ImageGrab.grabclipboard()
             #保存到项目中
             img.save("for-py/py-learn-code/code-blackChocolate/learn/day_5文件识别/picture.png")
-    for n in range(5):
-        print(sys.maxsize)
-screenShot()
+
+for n in range(sys.maxsize):      
+    print("第%d次截图"%(n+1))
+    screenShot()
+    #将图片转换成文本
+    str =  BaiDuAPI("for-py/py-learn-code/code-blackChocolate/learn/day_5文件识别/password.ini").picture2Texts("for-py/py-learn-code/code-blackChocolate/learn/day_5文件识别/picture.png")
+    print(str)
+    #将识别的文字添加到剪切板
+    GetTexts().setText(str)
+    flag = input()
+    flag = input("是否继续截图（Y/N）")
+    if flag == "N":
+        break
 '''
 print(sys.path)
+'''
---
+++ py-learn-code/code-blackChocolate/learn/day_6/learn_day_06.py
@@ -0,0 +1,3 @@
+import sys
+
+#在E:\python 3.6\Lib\site-packages目录下建立mypath.pth文件来保证模块导入的路径正确
