127b6080c9069b67745b127ab588735670944bbc
feathershine <zhl13779081529@hotmail.com>
2018-07-21 11:36:44 +0800

    2018-7-21 更新服务端

--- chatSystem/qqlite.py
+++ chatSystem/qqlite.py
@@ -3,11 +3,15 @@
 import os
 import wx
 
-import guiDesign.noname as noname
+# import guiDesign.noname as noname
 
-app = wx.App(False)
+# app = wx.App(False)
 
-mainwindow = noname.LoginWindow(None)
-mainwindow.Show(True)
+# mainwindow = noname.LoginWindow(None)
+# mainwindow.Show(True)
 
-app.MainLoop()
+# app.MainLoop()
+
+a = {'b': {'c': 1}}
+
+print(a['b']['c'])
